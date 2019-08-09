464882712f1abb1be4bcb7407ccdae3166c6a32a
Sharon Watkins <swatkins876@gmail.com>
2019-08-09 13:10:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,5 @@ reverse_each_word("Hello there, and how are you?")
 def reverse_each_word(sentence)
  sentence.reverse.split.reverse.join("Hi again, just making sure it's reversed!")
 end
+
+["Verifying that collect is being called."].collect { |sentence| puts sentence.Array.new }
