daad115f1c80f3a468ff5c42981bbe7cac939706
Sharon Watkins <swatkins876@gmail.com>
2019-08-09 13:02:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ def reverse_each_word(sentence)
  sentence.reverse.split.reverse.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you ?")
+#reverse_each_word("Hello there, and how are you ?")
