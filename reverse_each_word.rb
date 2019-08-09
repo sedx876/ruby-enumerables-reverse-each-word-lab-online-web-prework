d860c4fcacb32f842fc4015cc2b0889f65167f84
Sharon Watkins <swatkins876@gmail.com>
2019-08-09 13:07:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,5 +5,5 @@ end
 #reverse_each_word("Hello there, and how are you?")
 
 def reverse_each_word(sentence)
- sentence.reverse.split.reverse.join("Hello there, and how are you?")
+ sentence.reverse.split.reverse.join("Hi again, just making sure it's reversed!")
 end
